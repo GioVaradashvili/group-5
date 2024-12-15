4dc1c3a03c7ce555050d84c9263ad392b7bf1190
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-12-15 11:07:17 -0800

    1

--- day.01/homework/hw.py
+++
@@ -1,6 +0,0 @@
-name = "Giorgi"
-last_name = "Varadashvili"
-age = 15
-height = 1.80
-
-print(name + last_name + str(age) + str(height))
--- day.02/homework/hw.py
+++
@@ -1,6 +0,0 @@
-mothers_age = 42
-grandmothers_age = 62
-my_age = 15
-future = 10
-
-print(mothers_age - (my_age + future))
